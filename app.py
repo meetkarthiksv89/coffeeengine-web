6b46cbe17cec5d6f04846f71eee0652b2ce69c44
Pradhay <Pradhay@Pradhays-MacBook-Pro.local>
2020-06-03 20:27:38 +0100

    Trying to launch

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-import os, json
-from datetime import datetime
 from flask import Flask, session, redirect, render_template, request, jsonify, flash
 from flask_session import Session
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from werkzeug.security import check_password_hash, generate_password_hash
-import requests
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "new key"
