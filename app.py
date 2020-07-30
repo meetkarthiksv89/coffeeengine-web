92eea024bac3196beacd7ae52cc024a88038b175
Pradhay <Pradhay@Pradhays-MacBook-Pro.local>
2020-07-30 23:19:29 +0100

    changes

--- app.py
+++ app.py
@@ -14,14 +14,8 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 import pandas as pd
 import numpy as np
-from scipy.stats import randint
-import seaborn as sns # used for plot interactive graph.
-import matplotlib.pyplot as plt
-import seaborn as sns
-from io import StringIO
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import chi2
-from IPython.display import display
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
